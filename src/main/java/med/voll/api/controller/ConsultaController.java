4fa2a0a5ac7cf42a6845b808f7a70419c055870c
Alfredo <alfredozepeta@gmail.com>
2024-06-02 22:03:39 -0600

    Implementacion Reglas Negocio/Guardado Consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -2,8 +2,10 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultaService;
 import med.voll.api.domain.consulta.DatosAgendarConsulta;
 import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +14,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/consultas")
 public class ConsultaController {
 
+    @Autowired
+    private AgendaDeConsultaService service;
+
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DatosAgendarConsulta datos){
         System.out.println(datos);
+
+        service.agendar(datos);
 
         return ResponseEntity.ok(new DatosDetalleConsulta(null,null,null,null));
     }
